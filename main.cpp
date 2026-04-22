3f57acbea7ea79353fe5c18a40736ddb8c5f4e69
JVStu5634 <valdezjoseph123@gmail.com>
2026-04-21 22:02:12 -0500

    Update file input handling in main.cpp
    Fixed input file handling and adjusted loop condition.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,46 @@
+// Joseph Valdez
+// 4/21/2026
+// This program reads and displays the daily quiz scores of each student. The program then displays 
+// the highest and lowest scores overall.
+
 #include <iostream>
-#include <ifstream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct Class {
+string name;
+int score;
+};
+
+void displayData(Class students[], int size);
+void findMinMax(Class students[], int size, int& maxIdx, int& minIdx);
+
+int main() {
+
+const int SIZE = 10;
+Class students[SIZE];
+ifstream inFile;
+
+inFile.open("Input.txt");
+
+if (!inFile) {
+  cout << "Error: Cannot open input file "Input.txt" << endl;
+  return 1;
+  }
+
+for (int i = 0; i < SIZE; i++) {
+  if (!inFile >> students[i].name >> students[i].score)) {
+  break;
+  }
+}
+
+inFile.close();
+
+
+
+
+
+
